5782a700d0f12eecb963cd88c999e044f7683c82
Oleksandr Androsov <wdsasha22@icloud.com>
2023-10-20 19:16:06 +0100

    init

---
+++ stock/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+from
+# Register your models here.
---
+++ stock/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Stock(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    tags = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class User
