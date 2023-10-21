9dc652b0983208df674f42df67364b5b9878e5f6
Aleh <melnik.aleh.94@gmail.com>
2023-10-21 17:36:52 +0300

    add cors

--- config/settings.py
+++ config/settings.py
@@ -51,8 +51,10 @@ CORS_ALLOW_HEADERS = [
 CORS_ALLOWED_ORIGINS = [
 "http://localhost:8000",
 "http://localhost:5000",
+"http://localhost:3000",
 "http://127.0.0.1:8000"
-"http://127.0.0.1:5000"
+"http://127.0.0.1:5000",
+"http://127.0.0.1:3000"
 ]
 
 # Application definition
