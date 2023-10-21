ac922a7b72e65886ce2a6acbcb999078bf5198ae
Aleh <melnik.aleh.94@gmail.com>
2023-10-21 13:32:47 +0300

    change type

--- stock/data/reactions.py
+++ stock/data/reactions.py
@@ -6,7 +6,7 @@ from stock.models import Reaction, Stock
 
 class ReactionService:
     @staticmethod
-    def create_reaction(account_id: int, stock_id: int, reaction: str):
+    def create_reaction(account_id: str, stock_id: int, reaction: str):
         user = Account.objects.get_or_create(pk=account_id)
         stock = Stock.objects.get(pk=stock_id)
         reaction = Reaction.objects.create(
@@ -16,6 +16,6 @@ class ReactionService:
 
 
     @staticmethod
-    def get_user_reactions(user_id: int) -> List[Reaction]:
+    def get_user_reactions(user_id: str) -> List[Reaction]:
         user = Account.objects.get_or_create(pk=user_id)
         return Reaction.objects.filter(user=user)
