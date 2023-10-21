1d02217b2a58ac80ef234c34e287a180985e6d75
Aleh <melnik.aleh.94@gmail.com>
2023-10-21 13:02:25 +0300

    add ports

--- run_app.sh
+++ run_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py makemigrations  --settings=config.settings
 python manage.py migrate  --settings=config.settings
-python manage.py runserver
+python manage.py runserver 0.0.0.0:8000
