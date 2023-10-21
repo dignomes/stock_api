42ebd0d4397efdef8d18773c91d1af1b07b3cefb
Oleksandr Androsov <wdsasha22@icloud.com>
2023-10-21 17:03:07 +0100

    type

--- config/settings.py
+++ config/settings.py
@@ -55,8 +55,8 @@ CORS_ALLOWED_ORIGINS = [
 "http://localhost:3000",
 "http://127.0.0.1:8000"
 "http://127.0.0.1:5000",
-"http://127.0.0.1:3000"
-    "*"
+"http://127.0.0.1:3000",
+"*"
 ]
 
 # Application definition
